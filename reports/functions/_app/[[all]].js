3c0e97fba20a5bee821cee2dd02818225a1c1d94
Kevin Kong <39197277+the-kevin-kong@users.noreply.github.com>
2025-01-24 23:04:25 -0500

    test

--- reports/functions/_app/[[all]].js
+++ reports/functions/_app/[[all]].js
@@ -1,8 +1,22 @@
-export async function onRequestGet(ctx) {
-    const path = new URL(ctx.request.url).pathname.replace("/_app/", "");
-    const file = await ctx.env.APP.get(path);
-    if (!file) return new Response(null, { status: 404 });
+// export async function onRequestGet(ctx) {
+//     const path = new URL(ctx.request.url).pathname.replace("/_app/", "");
+//     const file = await ctx.env.APP.get(path);
+//     if (!file) return new Response(null, { status: 404 });
+//     return new Response(file.body, {
+//         headers: { "Content-Type": file.httpMetadata.contentType },
+//     });
+// }
+
+export async function onRequest(context) {
+    const url = new URL(context.request.url);
+    const filePath = url.pathname;
+    const file = await context.env.APP.get(filePath);
+    if (file === null) {
+      return new Response("Not found", { status: 404 });
+    }
     return new Response(file.body, {
-        headers: { "Content-Type": file.httpMetadata.contentType },
+      headers: {
+        'Content-Type': file.headers.get('Content-Type'),
+      },
     });
 }

72100e632a0db811e12abddba5b0a383e4cb0e1c
Kevin Kong <39197277+the-kevin-kong@users.noreply.github.com>
2025-01-24 21:56:42 -0500

    test

--- reports/functions/_app/[[all]].js
+++ reports/functions/_app/[[all]].js
@@ -1,5 +1,5 @@
 export async function onRequestGet(ctx) {
-    const path = new URL(ctx.request.url).pathname.replace("/build/_app/", "");
+    const path = new URL(ctx.request.url).pathname.replace("/reports/build/_app/", "");
     const file = await ctx.env.APP.get(path);
     if (!file) return new Response(`${path}`, { status: 404 });
     return new Response(file.body, {
